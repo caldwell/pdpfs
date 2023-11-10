6c442cfdf764ff118915b1b378c5339912cba148
David Caldwell <david@porkrind.org>
2023-11-10 13:20:16 -0800

    rt11fs.rs: Add framework for supporting copying files into the image (though that isn't written yet)

--- rt11fs.rs
+++ rt11fs.rs
@@ -1,7 +1,7 @@
 mod block;
 mod fs;
 
-use std::path::PathBuf;
+use std::path::{PathBuf, Path};
 
 use block::{BlockDevice, PhysicalBlockDevice};
 use block::imd::IMD;
@@ -17,19 +17,32 @@ const USAGE: &'static str = "
 Usage:
   rt11fs -h
   rt11fs [-h] -i <image> ls
-  rt11fs [-h] -i <image> cp <image-file> <local-destination>
+  rt11fs [-h] -i <image> cp <source-file> <dest-file>
 
 Options:
   -h --help              Show this screen.
   -i --image <image>     Use <image> as the disk image.
+
+ cp:
+   <source-file> and <dest-file> specify local (host) filesystem paths if they
+   contain a `/` character. Otherwise they specify files on the image. The
+   filenames will be converted to uppercase for convenience (but they will not be
+   truncated or stripped of other invalid characters).
+
+   Examples:
+     # This copies 'file.txt' from the local machine into disk image (as FILE.TXT):
+     rt11fs -i my_image.img cp ./file.txt file.txt
+
+     # This copies 'FILE.TXT' from the disk image into /tmp/FILE.TXT on the local machine:
+     rt11fs -i my_image.img cp FILE.TXT /tmp
 ";
 #[derive(Debug, Deserialize)]
 struct Args {
     flag_image:       PathBuf,
     cmd_ls:           bool,
     cmd_cp:           bool,
-    arg_image_file:   PathBuf,
-    arg_local_destination: PathBuf,
+    arg_source_file:  PathBuf,
+    arg_dest_file:    PathBuf,
 }
 fn main() -> anyhow::Result<()> {
     let args: Args = Docopt::new(USAGE)
@@ -53,21 +66,13 @@ fn with_physical_dev<P: PhysicalBlockDevice>(args: &Args, dev: P) -> anyhow::Res
     }
 
     if args.cmd_cp {
-        let local_dest = match (args.arg_local_destination.exists(), std::fs::metadata(&args.arg_local_destination)) {
-            (true, Ok(m)) if m.is_dir() => args.arg_local_destination.join(args.arg_image_file.file_name()
-                                                                               .ok_or(anyhow!("Bad filename: {}", args.arg_image_file.to_string_lossy()))?),
-            (true, Err(e)) => Err(e).with_context(|| format!("{}", args.arg_local_destination.to_string_lossy()))?,
-            (_, _) => args.arg_local_destination.clone(),
-        };
-        let source_file = args.arg_image_file.to_str().ok_or(anyhow!("Bad filename: {}", args.arg_image_file.to_string_lossy()))?
-            .to_uppercase();
-        let Some(file) = fs.file_named(&source_file) else {
-            return Err(anyhow!("File not found: {}", source_file));
-        };
-        print!("{} -> {}", file.name, local_dest.to_string_lossy());
-        let data = fs.image.block(file.block, file.length)?;
-        std::fs::write(local_dest, data.as_bytes())?;
-        print!("... Successfully copied {} blocks ({} bytes)\n", file.length, file.length * block::BLOCK_SIZE);
+        match (args.arg_source_file.components().count() > 1,
+               args.arg_dest_file.components().count() > 1) {
+            (false, true)  => cp_from_image(&fs, &args.arg_source_file, &args.arg_dest_file)?,
+            (true,  false) => cp_into_image(&fs, &args.arg_source_file, &args.arg_dest_file)?,
+            (false, false) => Err(anyhow!("Image to image copy is not supported yet."))?,
+            (true,  true)  => Err(anyhow!("Either the source or destination file needs to be on the image"))?,
+        }
     }
 
     Ok(())
@@ -77,4 +82,26 @@ fn ls<B: BlockDevice>(fs: &RT11FS<B>) {
     for f in fs.file_iter() {
         println!("{:10} {:>3}:{:<3} {:6} {}", f.creation_date.map(|d| d.to_string()).unwrap_or("<no-date>".to_string()), f.job, f.channel, f.length, f.name);
     }
+}
+
+fn cp_from_image<B: BlockDevice>(fs: &RT11FS<B>, src: &Path, dest: &Path) -> anyhow::Result<()> {
+    let local_dest = match (dest.exists(), std::fs::metadata(&dest)) {
+        (true, Ok(m)) if m.is_dir() => dest.join(src.file_name().ok_or(anyhow!("Bad filename: {}", src.to_string_lossy()))?),
+        (true, Err(e)) => Err(e).with_context(|| format!("{}", dest.to_string_lossy()))?,
+        (_, _) => dest.to_owned(),
+    };
+    let source_file = src.to_str().ok_or(anyhow!("Bad filename: {}", src.to_string_lossy()))?
+        .to_uppercase();
+    let Some(file) = fs.file_named(&source_file) else {
+        return Err(anyhow!("File not found: {}", source_file));
+    };
+    print!("{} -> {}", file.name, local_dest.to_string_lossy());
+    let data = fs.image.block(file.block, file.length)?;
+    std::fs::write(local_dest, data.as_bytes())?;
+    print!("... Successfully copied {} blocks ({} bytes)\n", file.length, file.length * block::BLOCK_SIZE);
+    Ok(())
+}
+
+fn cp_into_image<B: BlockDevice>(fs: &RT11FS<B>, src: &Path, dest: &Path) -> anyhow::Result<()> {
+    todo!()
 }
