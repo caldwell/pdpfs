1d86ac5971b12c299384d938f66e1038aeb51b3a
David Caldwell <david@porkrind.org>
2023-11-27 11:43:42 -0800

    fs.rs,rt11.rs: Move test helpers to fs.rs, so other filesystems can share.

--- fs.rs
+++ fs.rs
@@ -61,3 +61,69 @@ pub trait DirEntry : Debug {
 
     fn readonly(&self) -> bool;
 }
+
+
+#[cfg(test)]
+mod test { // No tests here, just helpful stuff that filesystem tests can use
+    use super::*;
+    use crate::block::PhysicalBlockDevice;
+
+    // Replacement for chrono::Local::now() so that tests are consistent
+    #[allow(non_snake_case)]
+    pub(crate) mod Local {
+        pub(crate) fn now() -> chrono::DateTime<chrono::FixedOffset> {
+            chrono::DateTime::<chrono::FixedOffset>::parse_from_rfc3339("2023-01-19 12:13:14+08:00").unwrap()
+        }
+    }
+
+    pub(crate) fn username() -> String { "test-user".into() }
+
+    pub(crate) struct TestDev(pub Vec<u8>);
+    impl BlockDevice for TestDev {
+        fn read_sector(&self, sector: usize) -> anyhow::Result<Vec<u8>> {
+            Ok(self.0[sector*512..(sector+1)*512].into())
+        }
+        fn write_sector(&mut self, sector: usize, buf: &[u8]) -> anyhow::Result<()> {
+            self.0.splice(sector*512..(sector+1)*512, buf.into_iter().map(|b| *b));
+            Ok(())
+        }
+        fn sector_size(&self) -> usize { 512 }
+        fn sectors(&self) -> usize { self.0.len()/512 }
+        fn physical_device(&self) -> Box<&dyn crate::block::PhysicalBlockDevice> {
+            Box::new(self)
+        }
+    }
+    impl PhysicalBlockDevice for TestDev {
+        fn geometry(&self) -> &crate::block::Geometry {unimplemented!()}
+        fn read_sector(&self, _cylinder: usize, _head: usize, _sector: usize) -> anyhow::Result<Vec<u8>> {unimplemented!()}
+        fn write_sector(&mut self, _cylinder: usize, _head: usize, _sector: usize, _buf: &[u8]) -> anyhow::Result<()> {unimplemented!()}
+        fn as_vec(&self) -> anyhow::Result<Vec<u8>> {unimplemented!()}
+        fn from_raw(_data: Vec<u8>, _geometry: crate::block::Geometry) -> Self { unimplemented!() }
+    }
+
+    #[macro_export] macro_rules! assert_block_eq {
+        ($image:expr, $block_num:expr, $( $expected_and_mask:expr ),*) => {
+            {
+                let got = $image.read_blocks($block_num, 1).expect(&format!("block {}", $block_num));
+                let mut expected_and_mask: Vec<u16> = Vec::new();
+                $( {
+                    expected_and_mask.extend($expected_and_mask.into_iter().map(|v| v as u16));
+                } )*
+                // We encode the inverse mask into the high byte of a u16--that way simple u8s are treated as 0xff.
+                // The ____ const, below, sets the high byte of the mask so that is becomes 0x00 when we invert it.
+                let expected: Vec<u8> = expected_and_mask.iter().map(|x| (x & 0xff) as u8).collect();
+                let mask: Vec<u8> = expected_and_mask.iter().map(|x| !(x >> 8) as u8).collect();
+                let masked: Vec<u8> = got.as_bytes().iter().zip(&mask).map(|(data, mask)| data & mask).collect();
+                if masked != expected {
+                    panic!("assertion `block {0} == expected` failed\n  Block {0} was:\n{1:?} \n Expected:\n{2:?}\n Mask:\n{3:?}",
+                        $block_num, got.as_bytes().hex_dump(), expected.hex_dump(), mask.hex_dump());
+                }
+            }
+        };
+    }
+    pub(crate) const ____: u16 = 0xff00;
+
+    pub(crate) fn incrementing(count: usize) -> Vec<u8> {
+        (0..count).map(|x| x as u8).collect::<Vec<u8>>()
+    }
+}
--- fs/rt11.rs
+++ fs/rt11.rs
@@ -13,10 +13,10 @@ use super::FileSystem;
 
 // Things we override to make testing easier
 #[cfg(not(test))] use chrono::Local;
-#[cfg    (test)]  use test::Local;
+#[cfg    (test)]  use super::test::Local;
 
 #[cfg(not(test))] use whoami::username;
-#[cfg    (test)]  use test::username;
+#[cfg    (test)]  use super::test::username;
 
 #[derive(Clone, Debug)]
 pub struct RT11FS<B: BlockDevice> {
@@ -758,64 +758,10 @@ impl<'a, B: BlockDevice> Drop for RT11FileWriter<'a, B> {
 #[cfg(test)]
 mod test {
     use super::*;
-    use crate::block::PhysicalBlockDevice;
+    use crate::fs::test::*;
+    use crate::assert_block_eq;
     use pretty_hex::PrettyHex;
     use std::io::Write;
-
-    // Replacement for chrono::Local::now() so that tests are consistent
-    #[allow(non_snake_case)]
-    pub(crate) mod Local {
-        pub(crate) fn now() -> chrono::DateTime<chrono::FixedOffset> {
-            chrono::DateTime::<chrono::FixedOffset>::parse_from_rfc3339("2023-01-19 12:13:14+08:00").unwrap()
-        }
-    }
-
-    pub fn username() -> String { "test-user".into() }
-
-    struct TestDev(Vec<u8>);
-    impl BlockDevice for TestDev {
-        fn read_sector(&self, sector: usize) -> anyhow::Result<Vec<u8>> {
-            Ok(self.0[sector*512..(sector+1)*512].into())
-        }
-        fn write_sector(&mut self, sector: usize, buf: &[u8]) -> anyhow::Result<()> {
-            self.0.splice(sector*512..(sector+1)*512, buf.into_iter().map(|b| *b));
-            Ok(())
-        }
-        fn sector_size(&self) -> usize { 512 }
-        fn sectors(&self) -> usize { self.0.len()/512 }
-        fn physical_device(&self) -> Box<&dyn crate::block::PhysicalBlockDevice> {
-            Box::new(self)
-        }
-    }
-    impl PhysicalBlockDevice for TestDev {
-        fn geometry(&self) -> &crate::block::Geometry {unimplemented!()}
-        fn read_sector(&self, _cylinder: usize, _head: usize, _sector: usize) -> anyhow::Result<Vec<u8>> {unimplemented!()}
-        fn write_sector(&mut self, _cylinder: usize, _head: usize, _sector: usize, _buf: &[u8]) -> anyhow::Result<()> {unimplemented!()}
-        fn as_vec(&self) -> anyhow::Result<Vec<u8>> {unimplemented!()}
-        fn from_raw(_data: Vec<u8>, _geometry: crate::block::Geometry) -> Self { unimplemented!() }
-    }
-
-    macro_rules! assert_block_eq {
-        ($image:expr, $block_num:expr, $( $expected_and_mask:expr ),*) => {
-            {
-                let got = $image.read_blocks($block_num, 1).expect(&format!("block {}", $block_num));
-                let mut expected_and_mask: Vec<u16> = Vec::new();
-                $( {
-                    expected_and_mask.extend($expected_and_mask.into_iter().map(|v| v as u16));
-                } )*
-                // We encode the inverse mask into the high byte of a u16--that way simple u8s are treated as 0xff.
-                // The ____ const, below, sets the high byte of the mask so that is becomes 0x00 when we invert it.
-                let expected: Vec<u8> = expected_and_mask.iter().map(|x| (x & 0xff) as u8).collect();
-                let mask: Vec<u8> = expected_and_mask.iter().map(|x| !(x >> 8) as u8).collect();
-                let masked: Vec<u8> = got.as_bytes().iter().zip(&mask).map(|(data, mask)| data & mask).collect();
-                if masked != expected {
-                    panic!("assertion `block {0} == expected` failed\n  Block {0} was:\n{1:?} \n Expected:\n{2:?}\n Mask:\n{3:?}",
-                        $block_num, got.as_bytes().hex_dump(), expected.hex_dump(), mask.hex_dump());
-                }
-            }
-        };
-    }
-    const ____: u16 = 0xff00;
 
     #[test]
     fn test_init() {
@@ -835,10 +781,6 @@ mod test {
                 _ => assert_block_eq!(fs.image, b, vec![0; 512]),
             }
         }
-    }
-
-    fn incrementing(count: usize) -> Vec<u8> {
-        (0..count).map(|x| x as u8).collect::<Vec<u8>>()
     }
 
     #[test]
